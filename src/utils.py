2fef4de26f9af9330c8835b64c8034ef96951323
Michael Djorup <michael.djorup@gmail.com>
2022-07-14 14:38:13 -0400

    CREATE: response builder function

---
+++ src/utils.py
@@ -0,0 +1,28 @@
+"""Module utils contains utility functions to be used across the application.
+"""
+import json
+
+from aws_lambda_powertools.event_handler.api_gateway import Response
+from aws_lambda_powertools.event_handler import content_types
+
+
+def build_response(status_code: int, body):
+    """
+    Summary:
+        Returns a standard response object used throughout the application
+
+    Parameters:
+        status_code (int): HTTP status code for the response
+        body: Response body
+
+    Returns:
+        Response: A response with the given status code and body
+    """
+    if not isinstance(status_code, int):
+        raise TypeError
+
+    return Response(
+        status_code=status_code,
+        content_type=content_types.APPLICATION_JSON,
+        body=json.dumps(body),
+    )
