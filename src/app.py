0d6632e64ba68234ae168c773cc2391454b6fd7b
Michael Djorup <michael.djorup@gmail.com>
2022-07-14 15:02:09 -0400

    ADD: middleware option, event logging

--- src/app.py
+++ src/app.py
@@ -9,6 +9,8 @@ from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.event_handler.exceptions import NotFoundError
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.middleware_factory import lambda_handler_decorator
 
 from src.utils import build_response
 
@@ -46,13 +48,40 @@ def handle_not_found(ex: NotFoundError):
     )
 
 
+@lambda_handler_decorator
+def middleware(event_handler, event, context):
+
+    # BEFORE
+
+    event["unauthorized"] = False
+
+    # HANDLER
+    response = event_handler(event, context)
+
+    # AFTER
+    return response
+
+
+@middleware
 @logger.inject_lambda_context(
     log_event=os.environ.get("LOG_EVENT", True),
     correlation_id_path=correlation_paths.API_GATEWAY_REST,
 )
 @tracer.capture_lambda_handler
-def handler():
+def handler(event: dict = None, context: LambdaContext = LambdaContext()):
     """
     Docstring
     """
-    return
+
+    logger.append_keys(
+        path=event.get("path"),
+        method=event.get("httpMethod"),
+        origin_ip=event.get("requestContext", {})
+        .get("identity", {})
+        .get("sourceIp", "UNAVAILABLE"),
+    )
+
+    if event.get("unauthorized", False):
+        return build_response(401, {"message": "Unauthorized"})
+
+    return app.resolve(event, context)
